05bb1de0b988f2ecae38d32d411f397deeaf349d
David Wise <dwise5@students.towson.edu>
2025-05-19 13:07:34 -0400

    Added artist and mood filtering to GET /api/songs endpoint"

--- src/main/java/com/blackcat/soundfolio/soundfolio/controller/SongController.java
+++ src/main/java/com/blackcat/soundfolio/soundfolio/controller/SongController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/songfolio/my-music/songs")
+@RequestMapping("/songfolio/my-music/")
 public class SongController {
 
     private final SongService songService;
