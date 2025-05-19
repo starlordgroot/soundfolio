9aef7a530697c9ceccef97e766f1479e37c0f535
David Wise <dwise5@students.towson.edu>
2025-05-19 10:11:14 -0400

    Spring project initialization.

---
+++ src/main/java/com/blackcat/soundfolio/soundfolio/SoundfolioApplication.java
@@ -0,0 +1,13 @@
+package com.blackcat.soundfolio.soundfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackcat/soundfolio/soundfolio/SoundfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcat.soundfolio.soundfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
